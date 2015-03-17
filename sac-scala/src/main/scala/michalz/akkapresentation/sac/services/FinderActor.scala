7cb557d003c7ae7ffd6117c0af4b442f8a255853
Michał Zając <michal.t.zajac@gmail.com>
2015-03-17 01:40:11 +0100

    Future from finder actor

--- sac-scala/src/main/scala/michalz/akkapresentation/sac/services/FinderActor.scala
+++ sac-scala/src/main/scala/michalz/akkapresentation/sac/services/FinderActor.scala
@@ -1,13 +1,18 @@
 package michalz.akkapresentation.sac.services
 
 import akka.actor.{Actor, ActorLogging, Props}
+import akka.pattern.pipe
 import michalz.akkapresentation.sac.domain.messages.{FoundAvailability, RequestAvailabilities}
 import michalz.akkapresentation.sac.services.finders.Finder
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * Created by michal on 16.03.15.
  */
 class FinderActor(private val finder: Finder) extends Actor with ActorLogging {
+
+  implicit val ec: ExecutionContext = context.dispatcher
 
   override def preStart = {
     log.info("Actor for {} created", finder.serviceName)
@@ -15,7 +20,10 @@ class FinderActor(private val finder: Finder) extends Actor with ActorLogging {
 
   def receive = {
     case RequestAvailabilities(postCode) => {
-        sender() ! FoundAvailability(postCode, finder.serviceId, finder.serviceAvailability(postCode))
+      val future = Future {
+        FoundAvailability(postCode, finder.serviceId, finder.serviceAvailability(postCode))
+      }
+      future pipeTo sender()
     }
   }
 }
